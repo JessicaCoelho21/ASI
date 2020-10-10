1b47d85aedf93ee8b449c5c9abbea01c682cd340
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-10 13:05:03 +0100

    ASI_FP_01 added

---
+++ ASI_FP_1/ASI_FP_1_ex2.py
@@ -0,0 +1,18 @@
+#Administração de Sistemas Informáticos, 1ª Semana, Exercício 2
+
+data = "8200123;Ana Maria;931221012;12/05/2000"
+
+#Criar um array para separar os dados com ';'
+array = data.split(";")
+print(array)
+
+#Tamanho do array
+print(len(array))
+
+#Adicionar 'SOL' ao array
+array.append("SOL")
+print(array)
+
+#Conversão da lista final para string
+convertion = ",".join(array)
+print(convertion)
