c47d618a72e01d717ca29fc67cb1c75fcc08ca07
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-12 18:06:25 +0100

    Worksheet 2 exercise 1 completed

--- ASI_FP_2/ASI_FP_2_ex1.py
+++ ASI_FP_2/ASI_FP_2_ex1.py
@@ -1,3 +1,39 @@
 #Semana 2, Exercício 1
 
-print("a resolver")
+idades = [16, 18, 10, 28, 24, 26, 30, 46, 72, 65, 91]
+
+#Idade da pessoa mais nova
+idades.sort()
+print("Idade da pessoa mais nova:")
+print(idades[0])
+
+#Idade da pessoa mais velha
+len(idades)
+print("Idade da pessoa mais velha:")
+print(idades[len(idades) - 1])
+
+#Média das idades
+soma = 0
+media = 0
+
+for idade in idades:
+    soma = soma + idade
+
+media = soma / len(idades)
+print("Média das idades:")
+print(media)
+
+#Média das idades dos maiores de 18 anos e menores de 65
+soma2 = 0
+media2 = 0
+count = 0
+
+for idade in idades:
+    if idade >= 18 and idade <= 65:
+        soma2 = soma2 + idade
+        count = count + 1
+
+media2 = soma2 / count
+print("Média das idades dos maiores de 18 anos e menores de 65:")
+print(media2)
+
