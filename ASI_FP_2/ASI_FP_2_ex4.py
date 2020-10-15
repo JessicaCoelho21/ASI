d0abdc92c3dd8876a68e8e8ea9d7f983288c7a18
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-15 15:34:57 +0100

    Worksheet 2, exercise 4 completed

--- ASI_FP_2/ASI_FP_2_ex4.py
+++ ASI_FP_2/ASI_FP_2_ex4.py
@@ -1,5 +1,18 @@
 #Semana 2, Exercício 3
 
+#Estrutura de dados que representa a tabela do enunciado
 translations = {"filme": ["film", "movie"], "locomotiva": ["locomotive", "train"],
                 "pessoa": ["person", "individual"]}
 
+#Imprimir a estrutura criada no ecrã
+for word, translation in translations.items():
+    print("Português: %s" % word)
+
+    for t in translation:
+        print("Tradução: %s" % t)
+
+    print()
+
+#Imprimir as traduções de "locomotiva"
+print("Português: locomotiva")
+print("Traduções: %s" % (", ".join(translations["locomotiva"])))
