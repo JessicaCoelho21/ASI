af8c80aa2eaa8ad0a1c20530675f796a8fdd5113
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-15 15:20:19 +0100

    Worksheet 2, exercise 3 completed

--- ASI_FP_2/ASI_FP_2_ex3.py
+++ ASI_FP_2/ASI_FP_2_ex3.py
@@ -1,4 +1,19 @@
 #Semana 2, Exercício 3
-
 dates = ['12/08/2010', '21/06/1950', '05/06/2005', '03/01/1981', '21/05/2014']
+
+age = 0
+
+for d in dates:
+    yearDates = (int)(d.split('/')[2])
+    age = 2020 - yearDates
+
+    if age >= 0 and age <= 12:
+        print("Criança: ", age, "anos")
+    elif age >= 13 and age <= 17:
+        print("Juvenil: ", age, "anos")
+    elif age >= 18 and age <= 64:
+        print("Adulto: ", age, "anos")
+    else:
+        print("Sénior: ", age, "anos")
+
 
