67f5127ecc6f8ed8e94fd1e3c08f637b64b33c0e
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-15 15:22:08 +0100

    Worksheet 2, exercise 3 completed

--- ASI_FP_2/ASI_FP_2_ex3.py
+++ ASI_FP_2/ASI_FP_2_ex3.py
@@ -1,11 +1,13 @@
 #Semana 2, Exercício 3
-dates = ['12/08/2010', '21/06/1950', '05/06/2005', '03/01/1981', '21/05/2014']
+import datetime
 
-age = 0
+dates = ['12/08/2010', '21/06/1950', '05/06/2005', '03/01/1981', '21/05/2014']
+year = datetime.date.today().year
 
 for d in dates:
     yearDates = (int)(d.split('/')[2])
-    age = 2020 - yearDates
+    age = year - yearDates
+    #age = 2020 - yearDates
 
     if age >= 0 and age <= 12:
         print("Criança: ", age, "anos")
