cd53af23f78b99a7c4f8f17642c71d9d2f4ba58f
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-20 16:06:06 +0100

    Worksheet 3 exercise 3 completed

---
+++ ASI_FP_3/ASI_FP_3_ex4.py
@@ -0,0 +1,11 @@
+# Semana 3, exercício 2
+
+# Ler para hashtables o conteúdo do ficheiro. A hashtable de Veiculos deverá ter como
+# chave o NIF e o valor será uma hashtable contendo a matricula como chave e o
+# valorIUC como valor. A hashtable Imoveis deverá ter como chave o NIF e o valor será
+# uma hashtable contendo o artigoMatricial e o valor será a taxaIMI.
+fp = open('dados2.txt', 'r')
+
+print("A resolver...")
+
+fp.close()
