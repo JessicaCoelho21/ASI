84ba266f523ad2c6cfdcbe84255375125c588a23
JessicaCoelho21 <jscoelho21@gmail.com>
2020-10-29 17:13:14 +0000

    Worksheet 4 exercise 1 completed

--- ASI_FP_4/ASI_FP_4_ex1.py
+++ ASI_FP_4/ASI_FP_4_ex1.py
@@ -1,3 +1,32 @@
 # Ficha de trabalho 4, exercício 1
+import sys
 
-print("A resolver...")
+# cálculo do preço final
+def calc1():
+    print("Introduza o valor correspondente ao preço base: ")
+    precoBase = sys.stdin.readline().strip()
+    precoFinal = "(" + precoBase + " * 23%) - 10"
+
+    print(precoFinal)
+
+    precoFinal = precoFinal.replace("%", " * 0.01 + " + precoBase)
+
+    print(precoFinal)
+    print("Preço final: ", (eval(precoFinal)))
+
+
+#versão 2
+def calc2():
+    print("Introduza o valor correspondente ao preço base: ")
+    precoBase = sys.stdin.readline().strip()
+    precoFinal = "(" + precoBase + " * 23%) - 10"
+
+    print(precoFinal)
+
+    precoFinal = precoFinal.replace("%", " / 100 + " + precoBase)
+
+    print(precoFinal)
+    print("Preço final: ", (eval(precoFinal)))
+
+calc1()
+calc2()
